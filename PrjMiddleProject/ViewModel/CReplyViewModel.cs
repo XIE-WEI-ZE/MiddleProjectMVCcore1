516bd1ca451412715bc28fc260dbc0a3d61124ee
JingFeng <tony821217@gmail.com>
2025-07-28 08:45:36 +0800

    20250728社群系統暫時完成

---
+++ PrjMiddleProject/ViewModel/CReplyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PrjMiddleProject.Models;
+using System.Collections.Generic;
+
+namespace PrjMiddleProject.ViewModel
+{
+    public class CReplyViewModel // 用於顯示文章和回覆列表
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public CommunityPost Post { get; set; }
+
+        public List<CommunityReply> Replies { get; set; } = new List<CommunityReply>();
+
+        // 用來存 MemberId 對應 MemberName
+        public Dictionary<int, string> MemberNamesMap { get; set; } = new Dictionary<int, string>();
+
+
+    }
+}
