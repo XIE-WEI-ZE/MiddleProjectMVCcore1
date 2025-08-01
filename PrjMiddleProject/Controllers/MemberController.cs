e26f2217a9bce471a062347b0ca3ff3be64c4916
謝維澤 <xie8610@gmail.com>
2025-08-01 12:20:06 +0800

    合併 dev 並還原本機修改

--- PrjMiddleProject/Controllers/MemberController.cs
+++ PrjMiddleProject/Controllers/MemberController.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrjMiddleProject.Models;
 using PrjMiddleProject.ViewModels;
+<<<<<<< Updated upstream
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+=======
+
+>>>>>>> Stashed changes
 
 namespace PrjMiddleProject.Controllers
 {
     public class MemberController : Controller
     {
+<<<<<<< Updated upstream
         public IActionResult List(CMemberKeywordViewModel vm)
         {
             NursingHomeContext db = new NursingHomeContext();
@@ -200,10 +205,90 @@ namespace PrjMiddleProject.Controllers
                 return RedirectToAction("List");
             // 切換帳號狀態
             member.Role = (member.Role == "Banned") ? "Member" : "Banned";
+=======
+
+
+        public IActionResult List() {
+            NursingHomeContext db = new NursingHomeContext();
+            IEnumerable<CMemberSimpleViewModel> members = null;
+            members = from m in db.Members
+                      select new CMemberSimpleViewModel
+                      {
+                          Name = m.Name,
+                          BirthDate = m.BirthDate,
+                          ResidesInCareHome = m.ResidesInCareHome
+                      };
+            return View(members);
+
+        }
+        public IActionResult Create() { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CMemberSimpleViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            using var db = new NursingHomeContext();
+
+            var member = new Member
+            {
+                Name = vm.Name,
+                BirthDate = vm.BirthDate,
+                ResidesInCareHome = vm.ResidesInCareHome
+            };
+
+            db.Members.Add(member);
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(int? id) {
+            if (id == null)
+                return RedirectToAction("List");
+            NursingHomeContext db = new NursingHomeContext();
+            var m = db.Members.FirstOrDefault(x=>x.MemberId == id);
+            if (m == null) return RedirectToAction("List");
+
+            return View(m);
+        
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Member m) {
+            NursingHomeContext db = new NursingHomeContext();
+            var member = db.Members.FirstOrDefault(x=>x.MemberId == m.MemberId);
+            if (member == null) return RedirectToAction("List");
+
+            member.Name = m.Name;
+            member.BirthDate = m.BirthDate;
+            member.ResidesInCareHome = m.ResidesInCareHome;
+>>>>>>> Stashed changes
             db.SaveChanges();
             return RedirectToAction("List");
+        }
+
+<<<<<<< Updated upstream
+
+    }
+}
+=======
+        public IActionResult Delete(int? id) { 
+            if(id == null) return RedirectToAction("List");
+            NursingHomeContext db = new NursingHomeContext();
+            var m = db.Members.FirstOrDefault(x => x.MemberId == id);
+            if (m != null)
+            { 
+                db.Members.Remove(m);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+            
         }
 
 
     }
 }
+>>>>>>> Stashed changes
